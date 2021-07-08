e983a53d065adead7f5715d99672401c9b241e97
Sharkman <scicolone.julian@gmail.com>
2021-07-08 17:49:24 -0300

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+from random import randint
+
+for i in range (0, 90):
+    for j in range (0,randint(1,7)):
+        d = str(i) + 'days ago'
+        with open('file.txt', 'a') as file:
+            file.write(d)
+        os.system('git add .')
+        os.system('git commit --date "'+ d +'" -m "commit"')
+os.system('git push -u origin main')
